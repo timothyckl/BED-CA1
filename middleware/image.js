2ec839245569baaaa0ea6f74e66827ae28a1a4ad
tim <89766111+timothyckl@users.noreply.github.com>
2021-11-27 20:00:10 +0800

    Refactor image validation feature

--- middleware/image.js
+++ middleware/image.js
@@ -2,9 +2,22 @@ const path = require('path');
 
 module.exports = {
     getFilePath: (profilePic) => {
+        // save current working directory to cwd
         const cwd = process.cwd();
         let filePath = `${cwd}/img/${profilePic.name}`;
         filePath = (path.relative(cwd, filePath)).split('\\').join('/');
         return filePath;
+    },
+    validateFile: ({ size, mimetype }, callback) => {
+        const imageFormat = mimetype.split('/')[1];
+        // size is in bytes, 1000000 = 1MB
+        const lessThan1MB = size < 1000000;
+        const correctFormat = imageFormat == ('jpg' || 'png');
+
+        if (!lessThan1MB) {
+            return callback('Image file must be less than 1MB. Try again.');
+        } else if (!correctFormat) {
+            return callback('Image file must be in jpg/png format. Try again.');
+        }
     }
 };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -28,21 +28,15 @@ userRouter.route('/')
             else if (data > 0) res.status(422).json({ error: 'Username/Email already exists. Try again.' });
             else {
                 const { profilePic } = req.files;
-                const { size } = profilePic;
-                const lessThan1MB = size < 1000000;
-                const imageFormat = profilePic.mimetype.split('/')[1];
-                const correctFormat = imageFormat == ('jpg' || 'png');
-                if (lessThan1MB) {
-                    if (correctFormat) {
-                        profilePic.mv(filePath, err => {
-                            if (err) res.status(500).json(err);
-                            else userTB.createOne(req.body, filePath, (err, data) => {
-                                if (err) res.status(500).json({ error: err });
-                                else res.status(201).json({ msg: `Success! Rows affected: ${data}` });
-                            });
+                image.validateFile(profilePic, err => {
+                    if (err) res.status(500).json({ error: err });
+                    else {
+                        userTB.createOne(req.body, filePath, (err, data) => {
+                            if (err) res.status(500).json({ error: err });
+                            else res.status(201).json({ msg: `Success! Rows affected: ${data}` });
                         });
-                    } else res.status(500).json({ error: `File must be in jpg/png format. Try again.` });
-                } else res.status(500).json({ error: `File must be less than 1MB. Try again.` });
+                    }
+                });
             }
         });
     });
