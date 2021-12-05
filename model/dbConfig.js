90776b35e04a43c5c90b47dff31876e08c223fd6
tim <89766111+timothyckl@users.noreply.github.com>
2021-12-06 00:05:40 +0800

    Add comments

--- model/dbConfig.js
+++ model/dbConfig.js
@@ -4,6 +4,7 @@ const mysql = require('mysql2');
 module.exports = {
     getConn: () => {
         return mysql.createConnection({
+            // edit options in .env file
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
             password: process.env.DB_PASS,
