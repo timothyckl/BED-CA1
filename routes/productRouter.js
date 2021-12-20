ca62fb0e2286fd15a9165ee2f019cf351ba34fcc
tim <89766111+timothyckl@users.noreply.github.com>
2021-12-20 22:38:10 +0800

    Add expected outcomes as comments

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -4,6 +4,24 @@ const productTB = require('../model/productTable');
 
 productRouter.route('/')
     .post((req, res) => {
+        // POST /product/
+        // Request body schema:
+        // {
+        //      "name": "SP AMD Ryzen 3600 Laptop",
+        //      "description": "Best Laptop",
+        //      “categoryid”: 1,
+        //      “brand”: “SP IT!”
+        //      "price”:”1855.50”
+        // }
+
+        // Success Code: 201
+        // Content: ID of the newly created listing
+        // {
+        //      "productid": 1
+        // }
+
+        // Error Code: 500
+        // Condition: Unknown Error
         productTB.createOne(req.body, (err, productid) => {
             if (err) res.status(500).json({ error: "Internal Server Error" });
             else res.status(201).json({ productid: productid });
@@ -12,6 +30,22 @@ productRouter.route('/')
 
 productRouter.route('/:id')
     .get((req, res) => {
+        // GET /product/:id/
+        // Request body schema: N/A
+
+        // Success Code: 200
+        // Content: Info of the matching product (including category name)
+        // {
+        //     "name": " SP AMD Ryzen 3600 Laptop",
+        //     "description": "Best Laptop",
+        //      “categoryid”: 1,
+        //      “categoryname”,“Laptop”,
+        //      “brand”: “SP IT!”
+        //     "price”:”1855.50”
+        // },
+
+        // Error Code: 500
+        // Condition: Unknown Error
         const { id } = req.params;
         if (isNaN(id)) res.status(500).json({ error: "Internal Server Error" })
         else {
@@ -23,6 +57,14 @@ productRouter.route('/:id')
         };
     })
     .delete((req, res) => {
+        // DELETE /product/:id/
+        // Request body schema: N/A
+
+        // Success Code: 204
+        // Content: N/A
+
+        // Error Code: 500
+        // Condition: Unknown Error
         const { id } = req.params;
         if (isNaN(id)) res.status(500).json({ error: "Internal Server Error" });
         else {
@@ -35,7 +77,22 @@ productRouter.route('/:id')
 
 productRouter.route('/:id/review')
     .post((req, res) => {
+        // POST /product/:id/review/
+        // Request body schema:
+        // {
+        //     “userid”:1,
+        //     “rating”:5
+        //     "review": “Good Laptop, super fast and can play game in class!”
+        // }
 
+        // Success Code: 201
+        // Content: ID of the newly created listing
+        // {
+        //     "reviewid": 1
+        // }
+
+        // Error Code: 500
+        // Condition: Unknown Error
     });
 
 module.exports = productRouter;
