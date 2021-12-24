8c548ad94ba9d14f062a6a940479949f9f494f52
tim <tim@DESKTOP-9552JHN.localdomain>
2021-12-25 00:20:28 +0800

    Remove line

--- routes/userInterestRouter.js
+++ routes/userInterestRouter.js
@@ -3,18 +3,6 @@ const userIntRouter = express.Router();
 const userIntTB = require('../model/userInterestTable');
 
 userIntRouter.post('/:userid', (req, res) => {
-    // POST /interest/:userid
-
-    // Request body schema:
-    // One or more category ids separated by “,” 
-    // {
-    // 	categoryids:1,2,4
-    // }
-
-    // Success code: 201
-
-    // Error code: 500
-    // Condition: Unknown error
     const { userid } = req.params;
     if (isNaN(userid)) res.status(500).json({ error: "Internal Server Error" });
     else {
